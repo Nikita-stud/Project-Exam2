d73dc3c1692f4cdee888f176a53059ddcc9394af
Nikita-stud <nikita151998de@gmail.com>
2026-06-17 16:50:40 +0200

    Additional folder structure, set up of ENDPOINT and ENV files

---
+++ src/components/helpers/PaginationControls.tsx
@@ -0,0 +1,28 @@
+export default function PaginationControls({
+  currentPage,
+  totalPages,
+  onPageChange,
+}: {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}) {
+  return (
+    <div className="d-flex gap-2">
+      <button
+        className="btn btn-primary text-white"
+        onClick={() => onPageChange(currentPage - 1)}
+        disabled={currentPage === 1 || totalPages === 0}
+      >
+        Previous
+      </button>
+      <button
+        className="btn btn-primary text-white"
+        onClick={() => onPageChange(currentPage + 1)}
+        disabled={currentPage === totalPages || totalPages === 0}
+      >
+        Next
+      </button>
+    </div>
+  );
+}
