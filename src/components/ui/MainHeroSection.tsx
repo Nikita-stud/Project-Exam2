a55cc5d1e5f4a92d8d9ea4dc00c28ba2802875c1
Nikita-stud <nikita151998de@gmail.com>
2026-06-23 06:25:21 +0200

    hero changes

--- src/components/ui/MainHeroSection.tsx
+++ src/components/ui/MainHeroSection.tsx
@@ -12,9 +12,19 @@ export default function MainHeroSection() {
         className="brightness-55 object-cover object-center"
       ></Image>
       <div className="absolute text-white bottom-[30%] left-[50px] max-w-[560px]">
-        <div className="">
+        <div>
           <h1 className="">Book your next stay today with HOLIDAZE</h1>
           <h2 className="flex justify-end">Choose the best one</h2>
+        </div>
+        <div className="absolute top-[-62px] w-[101px] h-[217px]">
+          <Image
+            src={`/hero-square.png`}
+            alt=""
+            role="representation"
+            fill
+            loading="eager"
+            className="object-fill"
+          />
         </div>
       </div>
     </section>
