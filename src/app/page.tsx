85eb91d3486098520db0f7ca1e940a96bb5336e0
Nikita-stud <nikita151998de@gmail.com>
2026-06-25 10:26:58 +0200

    Added daypicker for the solution I needed, made the search section with HTML and Tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import fetchVenues from '@/api/venues/fetchVenues';
 import VenueList from '@/components/venues/VenueList';
 import MainHeroSection from '@/components/ui/MainHeroSection';
+import VenueSearch from '@/components/venues/VenueSearch';
 
 export default async function Home() {
   const venues = await fetchVenues();
 
   return (
     <section>
-      <h1 className="pb-[20px] md:hidden">Book your stay</h1>
+      <VenueSearch />
+      <h1 className="pl-[20px] pt-[10px] md:hidden">Book your stay</h1>
       <MainHeroSection />
       <VenueList venues={venues} />
     </section>
