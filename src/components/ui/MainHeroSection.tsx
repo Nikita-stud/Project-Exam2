744ed50e60019a401a8b17e108948343c58a1005
Nikita-stud <nikita151998de@gmail.com>
2026-09-10 19:55:02 +0200

    WCAG

--- src/components/ui/MainHeroSection.tsx
+++ src/components/ui/MainHeroSection.tsx
@@ -13,8 +13,8 @@ export default function MainHeroSection() {
       ></Image>
       <div className="absolute text-white bottom-[30%] left-[50px] max-w-[560px]">
         <div>
-          <h1 className="">Book your next stay today with HOLIDAZE</h1>
-          <h2 className="flex justify-end">Choose the best one</h2>
+          <h1 className="font-bold">Book your next stay today with HOLIDAZE</h1>
+          <h2 className="flex justify-end font-bold">Choose the best one</h2>
         </div>
         <div className="absolute top-[-62px] w-[101px] h-[217px]">
           <Image
